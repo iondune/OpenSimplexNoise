7a8d99e1437d73ccd6b1528bf144875d775e25fa
Ian Dunn <iondune@gmail.com>
2015-03-22 03:00:13 -0700

    Some cleanup and organization

--- OpenSimplexNoise.h
+++ OpenSimplexNoise.h
@@ -423,13 +423,6 @@ namespace OSN {
 
 	};
 
-	// Array of gradient values for 2D. They approximate the directions to the
-	// vertices of a octagon from its center.
-	// Gradient set 2014-10-06.
-	const int Noise<2>::gradients [] = {
-		5, 2, 2, 5, -5, 2, -2, 5,
-		5, -2, 2, -5, -5, -2, -2, -5
-	};
 
 
 	// 3D Implementation of the OpenSimplexNoise generator.
@@ -1118,17 +1111,6 @@ namespace OSN {
 
 	};
 
-
-	// Array of gradient values for 3D. They approximate the directions to the
-	// vertices of a rhombicuboctahedron from its center, skewed so that the
-	// triangular and square facets can be inscribed in circles of the same radius.
-	// New gradient set 2014-10-06.
-	const int Noise<3>::gradients [] = {
-		-11, 4, 4, -4, 11, 4, -4, 4, 11, 11, 4, 4, 4, 11, 4, 4, 4, 11,
-		-11, -4, 4, -4, -11, 4, -4, -4, 11, 11, -4, 4, 4, -11, 4, 4, -4, 11,
-		-11, 4, -4, -4, 11, -4, -4, 4, -11, 11, 4, -4, 4, 11, -4, 4, 4, -11,
-		-11, -4, -4, -4, -11, -4, -4, -4, -11, 11, -4, -4, 4, -11, -4, 4, -4, -11
-	};
 
 
 	// 4D Implementation of the OpenSimplexNoise generator.
@@ -2584,30 +2566,6 @@ namespace OSN {
 	return (value * NORM_CONSTANT);
 		}
 
-	};
-
-
-	// Array of gradient values for 4D. They approximate the directions to the
-	// vertices of a disprismatotesseractihexadecachoron from its center, skewed so that the
-	// tetrahedral and cubic facets can be inscribed in spheres of the same radius.
-	// Gradient set 2014-10-06.
-	const int Noise<4>::gradients [] = {
-		3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3,
-		-3, 1, 1, 1, -1, 3, 1, 1, -1, 1, 3, 1, -1, 1, 1, 3,
-		3, -1, 1, 1, 1, -3, 1, 1, 1, -1, 3, 1, 1, -1, 1, 3,
-		-3, -1, 1, 1, -1, -3, 1, 1, -1, -1, 3, 1, -1, -1, 1, 3,
-		3, 1, -1, 1, 1, 3, -1, 1, 1, 1, -3, 1, 1, 1, -1, 3,
-		-3, 1, -1, 1, -1, 3, -1, 1, -1, 1, -3, 1, -1, 1, -1, 3,
-		3, -1, -1, 1, 1, -3, -1, 1, 1, -1, -3, 1, 1, -1, -1, 3,
-		-3, -1, -1, 1, -1, -3, -1, 1, -1, -1, -3, 1, -1, -1, -1, 3,
-		3, 1, 1, -1, 1, 3, 1, -1, 1, 1, 3, -1, 1, 1, 1, -3,
-		-3, 1, 1, -1, -1, 3, 1, -1, -1, 1, 3, -1, -1, 1, 1, -3,
-		3, -1, 1, -1, 1, -3, 1, -1, 1, -1, 3, -1, 1, -1, 1, -3,
-		-3, -1, 1, -1, -1, -3, 1, -1, -1, -1, 3, -1, -1, -1, 1, -3,
-		3, 1, -1, -1, 1, 3, -1, -1, 1, 1, -3, -1, 1, 1, -1, -3,
-		-3, 1, -1, -1, -1, 3, -1, -1, -1, 1, -3, -1, -1, 1, -1, -3,
-		3, -1, -1, -1, 1, -3, -1, -1, 1, -1, -3, -1, 1, -1, -1, -3,
-		-3, -1, -1, -1, -1, -3, -1, -1, -1, -1, -3, -1, -1, -1, -1, -3
 	};
 
 }
